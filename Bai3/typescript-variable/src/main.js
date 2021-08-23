2b7d411278b95c0d5f52177d756446a453ed0216
hung8695 <81769276+hung8695@users.noreply.github.com>
2021-08-23 11:17:48 +0700

    hihi

---
+++ Bai3/typescript-variable/src/main.js
@@ -0,0 +1,4 @@
+var a = 10;
+var b = 12;
+var area = a * b;
+console.log("Dien tich la " + area);
---
+++ Bai3/typescript-variable/src/main.ts
@@ -0,0 +1,4 @@
+let a : number = 10;
+let b: number = 12;
+let area : number = a*b;
+console.log(`Dien tich la ${area}`)
